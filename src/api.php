4a82ae5c983cba1510609db03af893c04586d3fc
Garciaadev <garciaadev@gmail.com>
2026-06-07 22:02:33 +0200

    fix: añadir charset UTF-8 en cabecera - versión owner

--- src/api.php
+++ src/api.php
@@ -1,5 +1,6 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/json; charset=UTF-8');
+// Versión Owner: cabecera con charset
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
 header('Access-Control-Allow-Headers: Content-Type');
